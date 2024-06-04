d3ea75e076008742d1227077d3c3cfd7d6e9d730
FrancescoUltron <159888261+FrancescoUltron@users.noreply.github.com>
2024-06-04 20:15:00 +0200

    Update radicequadrata.py

--- Programmazione/Python/radicequadrata.py
+++ Programmazione/Python/radicequadrata.py
@@ -1,6 +1,8 @@
-x = input()
-g = 3
-
-if g * g == x:
-    print("La radice quadrata di ", x , "e' ", g)
+def sqrt(x):
+    last_guess= x/2.0
+    while True:
+        guess= (last_guess + x/last_guess)/2
+        if abs(guess - last_guess) < .000001: # example threshold
+            return guess
+        last_guess= guess
 
